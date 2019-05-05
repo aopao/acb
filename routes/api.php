3aed74456740ab68f247b9d9869e7ee449ab197e
aopao <admin@wujinyu.com>
2019-05-05 14:07:07 +0800

    init

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', ['middleware' => 'api.throttle', 'limit' => 5000, 'expires' => 1], function ($api) {
+    $api->group(["namespace" => "App\Api\Controllers\Admin", 'prefix' => 'admin'], function ($api) {
+
+    });
+});
